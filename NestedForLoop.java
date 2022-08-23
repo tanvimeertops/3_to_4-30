8d38175c0cd61e4e6c475a42af6f59c82dbde856
tanvi <tanvimeer.tops@gmail.com>
2022-08-23 15:31:34 +0530

    21 aug

--- NestedForLoop.java
+++ NestedForLoop.java
@@ -1,13 +1,18 @@
+import java.lang.StackWalker.StackFrame;
+
 public class NestedForLoop {
     public static void main(String[] args) {
+        int i=1,sp=4;
         for (int row = 1; row<=5; row++) {
-            for (int col = 1; col<=row; col++) {
-                int i=1;
-                System.out.print(i);
-                i++;
+            for (int space = 1; space<=sp; space++) {
+                System.out.print(" ");
+                
             }
-
+            for (int star = 1; star<=row*2-1; star++) {
+                System.out.print("*");
+            }
             System.out.println();
+            sp--;
         }
 
     }
