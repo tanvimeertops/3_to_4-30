ca48d0d8550e88b0d93e49991798d44e374ba442
tanvi <tanvimeer.tops@gmail.com>
2022-09-01 16:37:04 +0530

    1 sep

---
+++ Examples.java
@@ -0,0 +1,26 @@
+public class Examples {
+    public static void main(String[] args) {
+        int a=0,b=1,sum=0;
+        //String str="mo";
+        int no= 121;
+        String str= Integer.toString(no);
+        
+        // while(no>0){
+        // int rem =no%10;
+        // no=no/10;
+        //     no.to
+        // }
+        
+        
+        // System.out.println(a);
+        // System.out.println(b);
+        // for (int i = 0; i<5; i++) {
+        //     sum=a+b;
+        //     a=b;
+        //     b=sum;
+            
+        //     System.out.println(sum);
+        // }
+
+    }
+}
